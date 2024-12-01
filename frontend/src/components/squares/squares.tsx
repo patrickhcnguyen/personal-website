dc39f9d42f976248303d0eedc224a67a109f8776
patrickhcnguyen <patricknguyen714@gmail.com>
2024-11-30 18:02:40 -0800

    added text

--- frontend/src/components/squares/squares.tsx
+++ frontend/src/components/squares/squares.tsx
@@ -6,11 +6,16 @@ const Squares = () => {
   const [yellowVisible, setYellowVisible] = useState(false);
   const [lightBrownVisible, setLightBrownVisible] = useState(false);
   const [redVisible, setRedVisible] = useState(false);
+  const [textVisible, setTextVisible] = useState(false);
 
   useEffect(() => {
     const greenTimer = setTimeout(() => {
       setGreenVisible(true);
     }, 300);
+
+    const textTimer = setTimeout(() => {
+      setTextVisible(true);
+    }, 450);
 
     const brownTimer = setTimeout(() => {
       setBrownVisible(true);
@@ -30,6 +35,7 @@ const Squares = () => {
 
     return () => {
       clearTimeout(greenTimer);
+      clearTimeout(textTimer);
       clearTimeout(brownTimer);
       clearTimeout(yellowTimer);
       clearTimeout(lightBrownTimer);
@@ -39,6 +45,48 @@ const Squares = () => {
 
   return (
     <div className="fixed inset-0 pointer-events-none">
+
+      {/* Green Square with Text */}
+      <div 
+        className={`
+          absolute 
+          w-[20.39vw] 
+          h-[26.68vh] 
+          left-[calc(10vw+15.08vw+0.234vw)]
+          top-[25.96vh]
+          bg-[#407A54]
+          transition-all 
+          duration-700
+          ease-in-out
+          origin-right
+          ${greenVisible 
+            ? 'opacity-100 scale-x-100' 
+            : 'opacity-0 scale-x-0'
+          }
+        `}
+      >
+        <div 
+          className={`
+            absolute
+            w-[9.375vw]
+            h-[10.697vh]
+            left-[0.5vw]
+            top-[17.02vh]
+            text-[#F5D0B3]
+            font-inria
+            font-bold
+            text-[2.5rem]
+            leading-tight
+            transition-opacity
+            duration-500
+            whitespace-nowrap
+            ${textVisible ? 'opacity-100' : 'opacity-0'}
+          `}
+        >
+          Hi, my<br />name is
+        </div>
+      </div>
+
       {/* Brown Rectangle */}
       <div 
         className={`
@@ -55,26 +103,6 @@ const Squares = () => {
           ${brownVisible 
             ? 'opacity-100 scale-y-100' 
             : 'opacity-0 scale-y-0'
-          }
-        `}
-      />
-
-      {/* Green Square */}
-      <div 
-        className={`
-          absolute 
-          w-[20.39vw] 
-          h-[26.68vh] 
-          left-[calc(10vw+15.08vw+0.234vw)]
-          top-[25.96vh]
-          bg-[#407A54]
-          transition-all 
-          duration-700
-          ease-in-out
-          origin-right
-          ${greenVisible 
-            ? 'opacity-100 scale-x-100' 
-            : 'opacity-0 scale-x-0'
           }
         `}
       />
@@ -97,7 +125,27 @@ const Squares = () => {
             : 'opacity-0 scale-x-0'
           }
         `}
-      />
+      >
+        <div 
+          className={`
+            absolute
+            w-full
+            h-full
+            flex
+            items-center
+            left-[0.5vw]
+            text-black
+            font-inria
+            font-bold
+            text-[2rem]
+            transition-opacity
+            duration-500
+            ${yellowVisible ? 'opacity-100' : 'opacity-0'}
+          `}
+        >
+          Patrick Nguyen
+        </div>
+      </div>
 
       {/* Light Brown Rectangle */}
       <div 
@@ -137,7 +185,29 @@ const Squares = () => {
             : 'opacity-0 scale-x-0'
           }
         `}
-      />
+      >
+        <div 
+          className={`
+            absolute
+            w-[14.69vw]
+            left-1/2
+            ml-[1vw]
+            text-[#BEAD8E]
+            font-inria
+            font-bold
+            text-[1rem]
+            leading-tight
+            transition-opacity
+            duration-500
+            ${redVisible ? 'opacity-100' : 'opacity-0'}
+          `}
+        >
+          I'm a 4th year student at UC Davis<br />
+          and fullstack SWE passionate<br />
+          about creating user driven<br />
+          products
+        </div>
+      </div>
     </div>
   );
 };
