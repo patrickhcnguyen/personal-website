49a29ba21ea2702cc0605e526189a5e891157e3b
patrickhcnguyen <patricknguyen714@gmail.com>
2024-11-30 23:49:50 -0800

    Experience section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import Navbar from "./components/navbar/navbar";
 import SocialBar from "./components/social/social-bar";
 import Squares from "./components/squares/squares";
 import AboutMe from "./components/about/aboutMe";
+import Experience from "./components/experience/experience";
 
 function App() {
+
   return (
     <div className="w-full m-0">
       <Navbar />
       <Squares />
       <AboutMe />
+      <Experience />
       <SocialBar />
     </div>
   );
