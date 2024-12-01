9028f93708b9db528c3f12bd39b44b6adc7b7365
patrickhcnguyen <patricknguyen714@gmail.com>
2024-11-30 18:44:52 -0800

    about me section included

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/navbar/navbar";
+import SocialBar from "./components/social/social-bar";
+import Squares from "./components/squares/squares";
+import AboutMe from "./components/about/aboutMe";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full m-0">
+      <Navbar />
+      <Squares />
+      <AboutMe />
+      <SocialBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
