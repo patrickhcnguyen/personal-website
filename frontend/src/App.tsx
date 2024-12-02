56e116c01643ce595896ca063be772b49b844d73
patrickhcnguyen <patricknguyen714@gmail.com>
2024-12-01 16:24:30 -0800

    mobile rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/navbar/navbar";
 import SocialBar from "./components/social/social-bar";
 import Squares from "./components/squares/squares";
@@ -6,17 +7,38 @@ import Experience from "./components/experience/experience";
 import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
 
+// Reuse the same useIsMobile hook
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkIsMobile();
+    window.addEventListener('resize', checkIsMobile);
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  return isMobile;
+};
+
 function App() {
+  const isMobile = useIsMobile();
 
   return (
     <div className="w-full m-0">
       <Navbar />
-      <Squares />
-      <AboutMe />
-      <Experience />
-      <Projects />
-      <Contact />
-      <SocialBar />
+      {!isMobile && (
+        <>
+          <Squares />
+          <AboutMe />
+          <Experience />
+          <Projects />
+          <Contact />
+          <SocialBar />
+        </>
+      )}
     </div>
   );
 }
