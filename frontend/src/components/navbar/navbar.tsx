9be7a5b15d50a1b1a6431ef6bd53a42339305274
patrickhcnguyen <patricknguyen714@gmail.com>
2024-12-01 17:54:44 -0800

    mobile jumping

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,22 +1,5 @@
 import { useState, useEffect } from "react";
-
-const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    checkIsMobile();
-
-    window.addEventListener('resize', checkIsMobile);
-
-    return () => window.removeEventListener('resize', checkIsMobile);
-  }, []);
-
-  return isMobile;
-};
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
